cb0047389016a043fc38e41fa724f26ddcd287d3
Hemanth <hemanth.kotagiri43@gmail.com>
2021-10-15 21:01:46 +0530

    Add urgent borders patch

--- config.def.h
+++ config.def.h
@@ -21,16 +21,19 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Hack:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+//static const char col_gray1[]       = "#222222";
+//static const char col_gray2[]       = "#444444";
+//static const char col_gray3[]       = "#bbbbbb";
+//static const char col_gray4[]       = "#eeeeee";
+//static const char col_cyan[]        = "#005577";
+//static const char col_urgborder[]   = "#ff0000";
+//static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+	//[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	//[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	//[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
+//};
+#include "/home/hemanth/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -71,11 +74,13 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon , "-fn", dmenufont};
 static const char *termcmd[]  = { "st", NULL };
 static const char *brave[]  = { "brave", NULL };
 static const char *emoji_launcher[]  = { "rofi", "-show", "emoji", NULL };
 static const char *naut[]  = { "nautilus", NULL };
+static const char *shutdown_menu[]  = { "shutdown_menu", NULL };
+static const char *whatsapp[]  = { "whatsapp-nativefier", NULL };
 
 /* Volume control */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
@@ -89,7 +94,9 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_e,      spawn,          {.v = emoji_launcher } },
+	{ ControlMask|Mod1Mask,         XK_Delete, spawn,          {.v = shutdown_menu } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = naut } },
+	{ MODKEY|ShiftMask,             XK_w,      spawn,          {.v = whatsapp } },
 	{ MODKEY,                       XK_n,      shiftview,      {.i = +1 }},
 	{ MODKEY,                       XK_p,      shiftview,      {.i = -1 }},
 	{ MODKEY,                       XK_b,      spawn,          {.v = brave }},
